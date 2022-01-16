c4edb6567f90de62d135ebadadf6fbc9ea5ce940
PLNA88 <paulina.gigowska@gmail.com>
2022-01-16 02:13:11 +0100

    ćwiczenie 2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    public static String Praktyki(boolean praktyki) {
+        String p;
+        if (praktyki==true)
+            p = "Zaakceptowane";
+        else
+            p = "Do wykonania";
+        return p;
+    }
 
     public static void main(String[] args) {
 
@@ -14,5 +22,37 @@ public class Main {
             c) wyświetlić dane z tablicy, użyć pętli for lub forEach
             d) dane powinny być estetycznie przedstawione
          */
+
+
+        Student x1 = new Student();
+        Student x2 = new Student();
+        Student x3 = new Student();
+
+        x1.imie = "Paulina";
+        x1.nazwisko = "Gigowska";
+        x1.id = 40801;
+        x1.praktyki = false;
+
+        x2.imie = "Anna";
+        x2.nazwisko = "Kowalska";
+        x2.id = 54729;
+        x2.praktyki = true;
+
+        x3.imie = "Adam";
+        x3.nazwisko = "Nowak";
+        x3.id = 82254;
+        x3.praktyki = false;
+
+        Student[] tablica1 = new Student[3];
+        tablica1[0] = x1;
+        tablica1[1] = x2;
+        tablica1[2] = x3;
+
+        for (Student i : tablica1
+        ) {
+            System.out.format("\n%-10s%-20s%-10s%-10s", i.imie, i.nazwisko, i.id, Praktyki(i.praktyki));
+        }
+
+
     }
 }
