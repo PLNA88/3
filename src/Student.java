1b72e274b1908b26dd14d56730c914f294322ba3
PLNA88 <paulina.gigowska@gmail.com>
2022-01-16 10:15:40 +0100

    WydzialEnum

---
+++ src/Student.java
@@ -0,0 +1,33 @@
+public class Student {
+    private Osoba osoba;
+    private WydzialEnum wydzial;
+
+    public Student(Osoba osoba, WydzialEnum wydzial) {
+        this.osoba = osoba;
+        this.wydzial = wydzial;
+    }
+
+
+    public Osoba getOsoba() {
+        return osoba;
+    }
+    public WydzialEnum getWydzial() {
+        return wydzial;
+    }
+
+    public void setOsoba(Osoba osoba) {
+        this.osoba = osoba;
+    }
+    public void setWydzial(WydzialEnum wydzial) {
+        this.wydzial = wydzial;
+    }
+
+
+    @Override
+    public String toString()
+    {return "Student {" +
+            osoba.toString() +
+            " Wydział: " + wydzial.toString() +
+            '}';
+    }
+}
