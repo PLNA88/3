f09c29d782db9024326883faea4ad32e32a9e4ab
PLNA88 <paulina.gigowska@gmail.com>
2022-01-16 09:56:58 +0100

    ćwiczenie 3

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) {
 
         /* ćw 3 klasy */
-
         /*  zad.1 - Najpierw należy uzupełnić kod w klasie Punkt.java,
             a) stworzyć objekt punktA typu Punkt,
             w konstruktorze zainicjować zmienną pX --> np. new Punkt(7), wykonać metodę punktA.toString();
@@ -19,5 +18,24 @@ public class Main {
             e) wywołać metody suma i róznica (obie wersje) z klasy punkt
         */
 
+
+        Punkt punktA = new Punkt(7);
+        System.out.println(punktA.toString());
+
+        punktA= new Punkt(1,2,3);
+        System.out.println(punktA.toString());
+
+        punktA.setpX(11);
+        punktA.setpY(22);
+        punktA.setpZ(33);
+        System.out.println(punktA.toString());
+
+        System.out.println(punktA.getpX());
+        System.out.println(punktA.getpY());
+        System.out.println(punktA.getpZ());
+
+        System.out.println(punktA.metodaSuma());
+        System.out.println(punktA.metodaRoznica());
+        System.out.println(punktA.metodaRoznica(111,222,333));
     }
 }
