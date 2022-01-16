abf1824c71a40b70e630943daa090a3d64398df3
PLNA88 <paulina.gigowska@gmail.com>
2022-01-16 09:50:24 +0100

    Punkt

--- src/Punkt.java
+++ src/Punkt.java
@@ -1,7 +1,6 @@
 /* klasa Punkt */
 public class Punkt {
     /* stworzyć zmienne prywatne pX, pY, pZ */
-
     /* stworzyć
         a) pusty konstruktor,
         b) konstruktor inicjalizujacy zmienną pX,
@@ -12,7 +11,6 @@ public class Punkt {
         podpowiedz ALT + INSERT można wybrać generowanie automatyczne
         konstruktora, getterów, setterów, nadpisywanie metody toString() i inne ...
     */
-
     /* Stworzyć metody
         a) suma - sumująca wszystkie wartości w klasie
         b) różnica - metody przeciążone roznica(){} i roznica(int x, int y, int z){}
@@ -20,4 +18,59 @@ public class Punkt {
             roznica(){ return pX - pY - pZ} i roznica(int x, int y, int z){ return pX*x - pY*y - pZ*z }
         d) nadpisać metodę toString(), aby zwracała wszystkie wartości pól, analogicznie jak w przykładzie
      */
+
+    private int pX, pY, pZ;
+
+    public Punkt() {
+    }
+
+    public Punkt(int pX) {
+        this.pX = pX;
+    }
+
+    public Punkt(int pX, int pY, int pZ) {
+        this.pX = pX;
+        this.pY = pY;
+        this.pZ = pZ;
+    }
+
+    public void setpX(int pX) {
+        this.pX = pX;
+    }
+    public void setpY(int pY) {
+        this.pY = pY;
+    }
+    public void setpZ(int pZ) {
+        this.pZ = pZ;
+    }
+
+    public int getpX() {
+        return pX;
+    }
+    public int getpY() {
+        return pY;
+    }
+    public int getpZ() {
+        return pZ * 10;
+    }
+
+
+    public int metodaSuma(){
+        return pX + pY + pZ;
+    }
+    public int metodaRoznica(){
+        return pX - pY - pZ;
+    }
+    public int metodaRoznica(int x, int y, int z){
+        return pX * x - pY * y - pZ * z;
+    }
+
+    @Override
+    public String toString() {
+        return "Punkt{" +
+                "x=" + pX +
+                ", y=" + pY +
+                ", z=" + pZ +
+                '}';
+    }
 }
